bfcf3fe01c0075bb444542637d0588f05a027dcb
ginavista <ginavist@gmail.com>
2022-07-06 19:50:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints Putchar
+ *
+ * descrption: prints _putchar
+ *
+ * return: 0 (succes)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchat('r');
+	_putchar('\n');
+
+	return (0);
+}
